31a375f96a26010607d8e0f6ea0497fd90c9877b
jochenschmidt27-max <jochen.schmidt27@gmail.com>
2026-03-04 07:19:04 +1300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -411,19 +411,6 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api.arn
-    format = jsonencode({
-      requestId      = "$context.requestId"
-      requestTime    = "$context.requestTime"
-      httpMethod     = "$context.httpMethod"
-      routeKey       = "$context.routeKey"
-      status         = "$context.status"
-      responseLength = "$context.responseLength"
-      ip             = "$context.identity.sourceIp"
-    })
-  }
-
   tags = local.tags
 }
 
